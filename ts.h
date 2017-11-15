1b96aa47bbae346c9e3940b212404903467c4427
aitor422 <aitorchorda@gmail.com>
2017-11-15 16:06:41 +0100

    inicio ts

--- ts.h
+++ ts.h
@@ -1,1 +1,17 @@
-ts
+#ifndf ts_aitor_miguel_h
+#define ts_aitor_miguel_h
+
+typedef enum {
+  VAR_INT,
+  VAR_REAL,
+  VAR_BOOL,
+  VAR_CHAR,
+  VAR_STRING
+} tipo_variable;
+
+typedef struct {
+  char *nombre;
+  tipo_variable tp;
+} variable;
+
+#endif
